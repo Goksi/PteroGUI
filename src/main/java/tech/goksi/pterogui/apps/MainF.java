b00100e40ba045979d957a3584c324ef88a745c0
Goksi <admin@goksi.co.uk>
2022-05-07 14:28:06 +0200

    Removed unused imports

--- src/main/java/tech/goksi/pterogui/apps/MainF.java
+++ src/main/java/tech/goksi/pterogui/apps/MainF.java
@@ -1,8 +1,6 @@
 package tech.goksi.pterogui.apps;
 
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import tech.goksi.pterogui.frames.MainFrame;
 
 import javax.swing.*;
