ed0e76f4dd74e4722a4b2c4906e2fd2c1b3281e6
Goksi <admin@goksi.co.uk>
2022-05-09 22:25:46 +0200

    Adding change of power state option

--- src/main/java/tech/goksi/pterogui/apps/ServerSettings.java
+++ src/main/java/tech/goksi/pterogui/apps/ServerSettings.java
@@ -1,5 +1,6 @@
 package tech.goksi.pterogui.apps;
 
+import com.mattmalec.pterodactyl4j.PteroAction;
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
 import com.mattmalec.pterodactyl4j.client.managers.WebSocketManager;
 import tech.goksi.pterogui.frames.ConsoleForm;
@@ -9,30 +10,50 @@ import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServerSettings {
     private final ClientServer server;
     private final MainF mainForm;
+    private ScheduledExecutorService executorService;
     public ServerSettings(ClientServer server, MainF mainForm){
         this.server = server;
         this.mainForm = mainForm;
+    }
+    enum State{
+        START,
+        STOP,
+        KILL,
+        RESTART
     }
 
     public void init(){
         JFrame jFrame = new JFrame("PteroGUI | " + server.getIdentifier());
         ServerSettingsFrame ssf = new ServerSettingsFrame();
         if(!server.isSuspended()){
-            server.retrieveUtilization().executeAsync(utilization -> {
-                ssf.getServerInfoLabel().setText(ssf.getServerInfoLabel().getText().replaceAll("%name", server.getName()).replaceAll("%id", server.getIdentifier())
-                        .replaceAll("%ip", server.getPrimaryAllocation().getIP() + ":" + server.getPrimaryAllocation().getPort())
-                        .replaceAll("%status", utilization.getState().name()).replaceAll("%egg", server.getEgg().getName()).replaceAll("%node", server.getNode()));
-            });
+            executorService = Executors.newSingleThreadScheduledExecutor();
+            executorService.scheduleAtFixedRate(()->{
+                server.retrieveUtilization().executeAsync(utilization -> {
+                    ssf.getServerInfoLabel().setText(ssf.getServerInfoLabel().getText().replaceAll("%name", server.getName()).replaceAll("%id", server.getIdentifier())
+                            .replaceAll("%ip", server.getPrimaryAllocation().getIP() + ":" + server.getPrimaryAllocation().getPort())
+                            .replaceAll("%egg", server.getEgg().getName()).replaceAll("%node", server.getNode())
+                            .replaceAll("RUNNING|OFFLINE|STARTING|STOPPING|%status", utilization.getState().name()));
+                    ssf.getMemoryUsageLbl().setText("Memory usage: %u / %a MB".replaceAll("%a", server.getLimits().getMemory()).
+                            replaceAll("%u", String.format("%.2f", (float)utilization.getMemory()/ 1024F / 1024F)));
+                });
+            }, 0, 5, TimeUnit.SECONDS);
+
         }else{
             ssf.getServerInfoLabel().setText(ssf.getServerInfoLabel().getText().replaceAll("%name", server.getName()).replaceAll("%id", server.getIdentifier())
                     .replaceAll("%ip", server.getPrimaryAllocation().getIP() + ":" + server.getPrimaryAllocation().getPort())
-                    .replaceAll("%status", "SUSPENDED").replaceAll("%egg", server.getEgg().getName()).replaceAll("%node", server.getNode()));
+                    .replaceAll("%egg", server.getEgg().getName()).replaceAll("%node", server.getNode()).replaceAll("%status", "SUSPENDED"));
+            ssf.getConsoleBtn().setEnabled(false);
+            ssf.getChangeStateBtn().setEnabled(false);
+            ssf.getStateComboBox().setEnabled(false);
         }
-        /*making whole console frame here, will add websocket impl*/
+        /*making whole console frame here*/
         ssf.getConsoleBtn().addActionListener(e ->{
             ssf.getConsoleBtn().setEnabled(false);
             JFrame console = new JFrame("PteroGUI | Console");
@@ -59,6 +80,24 @@ public class ServerSettings {
                 });
         /*end of console*/
 
+        ssf.getChangeStateBtn().addActionListener(e ->{
+            switch (ssf.getStateComboBox().getSelectedIndex()){
+                case 0: Objects.requireNonNull(switchState(State.START)).executeAsync(successful ->
+                        JOptionPane.showMessageDialog(mainForm.getMfFrame(), "Successfully sent START signal", "Action completed", JOptionPane.INFORMATION_MESSAGE));
+                        break;
+                case 1: Objects.requireNonNull(switchState(State.STOP)).executeAsync(successful ->
+                        JOptionPane.showMessageDialog(mainForm.getMfFrame(), "Successfully sent STOP signal", "Action completed", JOptionPane.INFORMATION_MESSAGE));
+                        break;
+                case 2: Objects.requireNonNull(switchState(State.KILL)).executeAsync(successful ->
+                        JOptionPane.showMessageDialog(mainForm.getMfFrame(), "Successfully sent KILL signal", "Action completed", JOptionPane.INFORMATION_MESSAGE));
+                        break;
+                case 3: Objects.requireNonNull(switchState(State.RESTART)).executeAsync(successful ->
+                        JOptionPane.showMessageDialog(mainForm.getMfFrame(), "Successfully sent RESTART signal", "Action completed", JOptionPane.INFORMATION_MESSAGE));
+            }
+                });
+
+
+
 
 
 
@@ -66,6 +105,7 @@ public class ServerSettings {
         jFrame.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/cool.png"))).getImage());
         jFrame.setResizable(false);
         jFrame.pack();
+        jFrame.setLocationRelativeTo(mainForm.getMfFrame());
         jFrame.setVisible(true);
 
 
@@ -75,8 +115,19 @@ public class ServerSettings {
             public void windowClosing(WindowEvent e) {
                 jFrame.dispose();
                 mainForm.getMfFrame().setVisible(true);
+                if(executorService != null &&!executorService.isShutdown())executorService.shutdown();
             }
         });
 
+    }
+
+    private PteroAction<Void> switchState(State state){
+        switch (state){
+            case KILL: return server.kill();
+            case STOP: return server.stop();
+            case START: return server.start();
+            case RESTART: return server.restart();
+        }
+        return null;
     }
 }
--- src/main/java/tech/goksi/pterogui/frames/ServerSettingsFrame.java
+++ src/main/java/tech/goksi/pterogui/frames/ServerSettingsFrame.java
@@ -23,11 +23,26 @@ public class ServerSettingsFrame extends JPanel {
         return consoleBtn;
     }
 
+    public JButton getChangeStateBtn() {
+        return changeStateBtn;
+    }
+
+    public JComboBox<String> getStateComboBox() {
+        return stateComboBox;
+    }
+
+    public JLabel getMemoryUsageLbl() {
+        return memoryUsageLbl;
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         label1 = new JLabel();
         label2 = new JLabel();
         consoleBtn = new JButton();
+        changeStateBtn = new JButton();
+        stateComboBox = new JComboBox<>(new String[]{"Start", "Stop", "Kill", "Restart"});
+        memoryUsageLbl = new JLabel();
 
         //======== this ========
 
@@ -40,28 +55,51 @@ public class ServerSettingsFrame extends JPanel {
         //---- consoleBtn ----
         consoleBtn.setText("Open console");
 
+        //---- changeStateBtn ----
+        changeStateBtn.setText("Change state");
+
+        //---- memoryUsageLbl ----
+        memoryUsageLbl.setText("Memory usage: %u/%a MB");
+
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup()
                 .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(consoleBtn)
-                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 118, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup()
+                        .addGroup(layout.createSequentialGroup()
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addComponent(consoleBtn, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(changeStateBtn, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(stateComboBox, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+                        .addComponent(memoryUsageLbl))
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createParallelGroup()
                         .addComponent(label2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                         .addComponent(label1))
-                    .addGap(25, 25, 25))
+                    .addGap(65, 65, 65))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
-                    .addGap(16, 16, 16)
-                    .addComponent(label1)
+                    .addGroup(layout.createParallelGroup()
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(16, 16, 16)
+                            .addComponent(label1))
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(memoryUsageLbl)))
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                     .addGroup(layout.createParallelGroup()
                         .addComponent(label2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                        .addComponent(consoleBtn))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(consoleBtn)
+                            .addGap(18, 18, 18)
+                            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(changeStateBtn)
+                                .addComponent(stateComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
                     .addContainerGap(110, Short.MAX_VALUE))
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
@@ -71,6 +109,9 @@ public class ServerSettingsFrame extends JPanel {
     private JLabel label1;
     private JLabel label2;
     private JButton consoleBtn;
+    private JButton changeStateBtn;
+    private JComboBox<String> stateComboBox;
+    private JLabel memoryUsageLbl;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 
 }
