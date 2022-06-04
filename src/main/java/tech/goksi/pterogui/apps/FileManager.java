2ce0f8616bd3e4ae554c051ae200bc3546b8d25b
Goksi <admin@goksi.co.uk>
2022-06-04 17:59:11 +0200

    Update file manager logic

--- src/main/java/tech/goksi/pterogui/apps/FileManager.java
+++ src/main/java/tech/goksi/pterogui/apps/FileManager.java
@@ -1,22 +1,19 @@
 package tech.goksi.pterogui.apps;
 
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
-import com.mattmalec.pterodactyl4j.client.entities.Directory;
 import com.mattmalec.pterodactyl4j.client.entities.GenericFile;
 
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-
+import java.util.Objects;
 
 
 public class FileManager {
+    public static boolean STOP_RECURSIVE = false;  //really can't think of another way rn
     private final JTree tree;
     private final ClientServer server ;
-    private DefaultTreeModel model;
-
-
     public FileManager(JTree tree, ClientServer server){
         this.tree = tree;
         this.server = server;
@@ -25,55 +22,34 @@ public class FileManager {
 
     public void updateUI(){
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(server.getName());
+        server.retrieveDirectory().executeAsync(f -> updateFiles(f, root));
 
-
-        server.retrieveDirectory().executeAsync(e -> e.getFiles().forEach(f ->{
-            addFiles("/", f);
-
-            if(f.getMimeType().equals("inode/directory")){
-                DefaultMutableTreeNode dir = new DefaultMutableTreeNode(f.getName());
-
-                server.retrieveDirectory(f.getName()).executeAsync(dirFile -> {
-
-
-                    dirFile.getFiles().forEach(file ->{
-                        //nesto ovde idk
-                        dir.add(new DefaultMutableTreeNode(file.getName()));
-                    }); //mozda neka rekuzivna funkcija ?
-
-                });
-
-                root.add(dir);
-
-
-            }else {
-                root.add(new DefaultMutableTreeNode(f.getName()));
-            }
-        }));
-        model = new DefaultTreeModel(root);
+        DefaultTreeModel model = new DefaultTreeModel(root);
 
         tree.setModel(model);
     }
-
-    private void addFiles(String directory, GenericFile f){
-        DefaultMutableTreeNode dire = new DefaultMutableTreeNode(directory);
-
-        if(f.getMimeType().equals("inode/directory")){
-            server.retrieveDirectory(directory).executeAsync(dir -> {
-
-
-                dir.getFiles().forEach(file -> {
-                    System.out.println(file.getName());
-                    addFiles(file.getName(), file);
+   /*problem ako se izadje dok fajlovi nisu skroz ucitani*/
+    private void updateFiles(GenericFile file, DefaultMutableTreeNode lastNode){
+        if(STOP_RECURSIVE) return; //little faster, but it still freezes (probably will add async)
+        if(!file.isFile()){
+            DefaultMutableTreeNode dir;
+            if(!Objects.equals(file.getName(), "Root Directory")){
+                dir = new DefaultMutableTreeNode(file.getName());
+            }else {
+                dir = lastNode;
+            }
+            server.retrieveDirectory(file.getPath()).executeAsync(subFolder -> {
+                if(!Objects.equals(subFolder.getName(), file.getName())){
+                    dir.add(new DefaultMutableTreeNode(subFolder.getName()));
+                }
+                subFolder.getFiles().forEach(files -> {
+                    updateFiles(files, dir);
                 });
-
-
             });
+            if(!Objects.equals(dir, lastNode)) lastNode.add(dir);
         }else {
-            dire.add(new DefaultMutableTreeNode(f.getName()));
+            lastNode.add(new DefaultMutableTreeNode(file.getName()));
         }
-
-
     }
-
+    /*TODO: metode za otvaranje, i brisanje fajla*/
 }
