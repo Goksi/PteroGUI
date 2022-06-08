a3f8f05e32923366bce494c5c362798bcfe9bbe5
Goksi <admin@goksi.co.uk>
2022-06-08 15:51:53 +0200

    Added close button & getters

--- src/main/java/tech/goksi/pterogui/frames/FileEditPanel.java
+++ src/main/java/tech/goksi/pterogui/frames/FileEditPanel.java
@@ -20,11 +20,20 @@ public class FileEditPanel extends JPanel {
         return textArea1;
     }
 
+    public JButton getButton2() {
+        return button2;
+    }
+
+    public JButton getButton1() {
+        return button1;
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         button1 = new JButton();
         scrollPane1 = new JScrollPane();
         textArea1 = new JTextArea();
+        button2 = new JButton();
 
         //======== this ========
         setPreferredSize(new Dimension(500, 340));
@@ -37,6 +46,9 @@ public class FileEditPanel extends JPanel {
             scrollPane1.setViewportView(textArea1);
         }
 
+        //---- button2 ----
+        button2.setText("Close");
+
         GroupLayout layout = new GroupLayout(this);
         setLayout(layout);
         layout.setHorizontalGroup(
@@ -44,9 +56,10 @@ public class FileEditPanel extends JPanel {
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(layout.createParallelGroup()
-                        .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 473, Short.MAX_VALUE)
-                        .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addGap(0, 338, Short.MAX_VALUE)
+                        .addComponent(scrollPane1, GroupLayout.DEFAULT_SIZE, 465, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(button2, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 193, Short.MAX_VALUE)
                             .addComponent(button1, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)))
                     .addContainerGap())
         );
@@ -56,7 +69,9 @@ public class FileEditPanel extends JPanel {
                     .addContainerGap()
                     .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 292, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(button1)
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(button1)
+                        .addComponent(button2))
                     .addContainerGap())
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
@@ -66,5 +81,6 @@ public class FileEditPanel extends JPanel {
     private JButton button1;
     private JScrollPane scrollPane1;
     private JTextArea textArea1;
+    private JButton button2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
