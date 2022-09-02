b8b91ee612d0dc05947ff94311b6cd24357eff9c
Goksi <goksi1312@gmail.com>
2022-09-02 02:22:27 +0200

    Create StringUtils.java

---
+++ src/main/java/tech/goksi/pterogui/utils/StringUtils.java
@@ -0,0 +1,37 @@
+package tech.goksi.pterogui.utils;
+
+public class StringUtils {
+
+    public static String difference(String str1, String str2) {
+        if (str1 == null) {
+            return str2;
+        }
+        if (str2 == null) {
+            return str1;
+        }
+        int at = indexOfDifference(str1, str2);
+        if (at == -1) {
+            return "";
+        }
+        return str2.substring(at);
+    }
+
+    private static int indexOfDifference(CharSequence cs1, CharSequence cs2) {
+        if (cs1 == cs2) {
+            return -1;
+        }
+        if (cs1 == null || cs2 == null) {
+            return 0;
+        }
+        int i;
+        for (i = 0; i < cs1.length() && i < cs2.length(); ++i) {
+            if (cs1.charAt(i) != cs2.charAt(i)) {
+                break;
+            }
+        }
+        if (i < cs2.length() || i < cs1.length()) {
+            return i;
+        }
+        return -1;
+    }
+}
